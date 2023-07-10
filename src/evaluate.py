a3373076353f76b8b883648994105c78bde874c0
NotXia <35894453+NotXia@users.noreply.github.com>
2023-07-10 10:23:54 +0200

    Add LEAD-n in evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -22,33 +22,33 @@ import sys
         splits : str[]
             Splits of the dataset to use
 
-        strategy : str
-            See BaseSummarizer.
-
-        strategy_args : any
-            See BaseSummarizer.
+        args : any
+            Additional arguments.
 """
-def evaluate(model, datasets, splits, strategy, strategy_args):
+def evaluate(model, datasets, splits, args={}):
     metrics = MetricsLogger()
 
-    if type(model) != str: model.eval()
-    with torch.no_grad():
-        for split in splits:
-            for i, document in enumerate(datasets[split]):
-                summary = ""
+    for split in splits:
+        for i, document in enumerate(datasets[split]):
+            summary = ""
 
-                if model == "oracle":
-                    selected_sents = [sent for i, sent in enumerate(document["sentences"]) if document["labels"][i]]
+            if model == "oracle":
+                selected_sents = [sent for i, sent in enumerate(document["sentences"]) if document["labels"][i]]
+                summary = "\n".join(selected_sents)
+            elif model == "lead":
+                selected_sents = document["sentences"][:args["lead"]]
+                summary = "\n".join(selected_sents)
+            else:
+                model.eval()
+                with torch.no_grad():
+                    selected_sents, _ = model.summarizeSentences(document["sentences"], args["strategy"], args["strategy_args"])
                     summary = "\n".join(selected_sents)
-                else:
-                    selected_sents, _ = model.summarizeSentences(document["sentences"], strategy, strategy_args)
-                    summary = "\n".join(selected_sents)
 
-                metrics.add("rouge", evalROUGE( [document["ref_summary"]], [summary] ))
-                sys.stdout.write(f"\r{i+1}/{len(datasets[split])} ({split}) --- {metrics.format(['rouge'])}")
-                sys.stdout.flush()
+            metrics.add("rouge", evalROUGE( [document["ref_summary"]], [summary] ))
+            sys.stdout.write(f"\r{i+1}/{len(datasets[split])} ({split}) --- {metrics.format(['rouge'])}\033[K")
+            sys.stdout.flush()
 
-    sys.stdout.write(f"\r")
+    sys.stdout.write("\r\033[K")
     print(f"{metrics.format(['rouge'])}")
 
 
@@ -58,6 +58,7 @@ if __name__ == "__main__":
     model_args = parser.add_mutually_exclusive_group(required=True)
     model_args.add_argument("--checkpoint", type=str, help="Path to the checkpoint of the model")
     model_args.add_argument("--oracle", action="store_true", help="Evaluate using the oracle (greedy selection)")
+    model_args.add_argument("--lead", type=int, help="Evaluate using LEAD-n (select first n sentences)")
     parser.add_argument("--dataset", type=str, required=True, help="Path to a preprocessed extractive dataset")
     parser.add_argument("--splits", type=str, default="test", required=True, help="Splits of the dataset to use for the evaluation (e.g. test,validation)")
     strategy_args = parser.add_mutually_exclusive_group(required="--checkpoint" in sys.argv)
@@ -73,40 +74,41 @@ if __name__ == "__main__":
     torch.use_deterministic_algorithms(mode=True)
     if torch.cuda.is_available(): os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 
-    print("-- Loading model --")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     datasets = load_from_disk(args.dataset)
     model = None
+    eval_args = {}
     if args.checkpoint is not None:
+        print("-- Loading model --")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         checkpoint = torch.load(args.checkpoint, map_location=device)
         model = loadModel(checkpoint["model_name"]).to(device)
         model.load_state_dict(checkpoint["model_state_dict"])
+
+        if args.strategy_length is not None:
+            eval_args["strategy"] = "length"
+            eval_args["strategy_args"] = args.strategy_length
+        elif args.strategy_count is not None:
+            eval_args["strategy"] = "count"
+            eval_args["strategy_args"] = args.strategy_count
+        elif args.strategy_ratio is not None:
+            eval_args["strategy"] = "ratio"
+            eval_args["strategy_args"] = args.strategy_ratio
+        elif args.strategy_threshold is not None:
+            eval_args["strategy"] = "threshold"
+            eval_args["strategy_args"] = args.strategy_threshold
+        else:
+            raise NotImplementedError
     elif args.oracle:
         model = "oracle"
-    
-    strategy = None
-    strategy_args = None
-    if args.strategy_length is not None:
-        strategy = "length"
-        strategy_args = args.strategy_length
-    elif args.strategy_count is not None:
-        strategy = "count"
-        strategy_args = args.strategy_count
-    elif args.strategy_ratio is not None:
-        strategy = "ratio"
-        strategy_args = args.strategy_ratio
-    elif args.strategy_threshold is not None:
-        strategy = "threshold"
-        strategy_args = args.strategy_threshold
-    else:
-        raise NotImplementedError
-    
+    elif args.lead != None:
+        model = "lead"
+        eval_args["lead"] = args.lead
+
 
     print("-- Starting evaluation --")
     evaluate(
         model = model,
         datasets = datasets,
         splits = args.splits.split(","),
-        strategy = strategy,
-        strategy_args = strategy_args
+        args = eval_args
     )
